249c1483ee75e64a3f88d4abb8861c59528db6cb
Cheryet <C.heryet@outlook.com>
2022-12-12 13:49:43 -0700

    update logic for game

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 # ~ Requires ~
-require './classes/player'
-require './classes/game'
-require './classes/question'
+require './player'
+require './game'
+require './question'
 
 # ~ Math Game ~
 
@@ -18,3 +18,36 @@ player2 = Player.new(player2_name)
 #Confirm players object is populating
 # p player1
 # p player2
+
+# Start new Game
+new_game = Game.new(player1, player2)
+
+# p new_game
+
+# Game Logic
+while new_game.players[0].lifes > 0 and new_game.players[1].lifes > 0 do
+  question = Questions.new
+
+  puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  puts "#{new_game.current_player.name}'s turn!"
+  print "#{question.new_question}"
+  
+  answer = question.answer
+  player_answer = gets.chomp.to_i
+
+  if player_answer != answer
+    puts "Whoops! That wasn't right"
+    new_game.remove_life
+  else
+    puts 'Correct!'
+  end
+
+
+  new_game.report_lifes
+
+  new_game.change_player
+
+end
+
+
+
